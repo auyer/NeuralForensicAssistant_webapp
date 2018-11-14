e06c670e7d2b74bfb5e79d4f298f6112002157cd
Rafael Passos <rafael@rcpassos.me>
2018-11-14 01:13:59 -0200

    List template and Vuetify
    Added vuetify dependencies and a better login form
    
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 import App from './App';
 import router from './router';
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import Login from '@/components/Login';
 import Secure from '@/components/Secure';
+import List from '@/components/List';
 
 Vue.use(Router);
+Vue.use(Vuetify);
 
 export default new Router({
   routes: [
@@ -20,6 +23,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List,
     },
     {
       path: '/secure',
