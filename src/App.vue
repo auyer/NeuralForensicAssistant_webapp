10ba8c64a66272398ac0e6478c659688e54224e8
Rafael Passos <rafael@rcpassos.me>
2018-11-14 01:10:04 -0200

    dynamic toolbar
    added a dynamic toolbar to all pages.
    
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-	<div id="app">
-		 <!-- <div id="nav">
-						
+  <v-app id="app" >
+      <!-- <router-view/> -->
+			<!-- <span>Forensic Deep Learning</span> -->
+			<v-toolbar>
+				<!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+				<v-toolbar-title>Forensic Deep Learning</v-toolbar-title>
+				<v-spacer></v-spacer>
+				<v-toolbar-items class="hidden-sm-and-down">
+				<div v-if="authenticated">
+				<router-link to='/list' replace><v-btn flat>Cases</v-btn></router-link>
+				<router-link to="/login" v-on:click.native="logout()" replace><v-btn flat>Logout</v-btn></router-link>
 				</div>
-				 -->
-		<header>
-			<span>Forensic Deep Learning</span>
-			<router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace> <button class="button buttonGray">Logout</button></router-link>
-		</header>
+				<!-- <v-btn flat>Link Three</v-btn> -->
+				</v-toolbar-items>
+			</v-toolbar>
 		<main>
 			<router-view @authenticated="setAuthenticated"></router-view>
 		</main>
-	</div>
+  </v-app>
 </template>
 
 <script>
 import Vue from 'vue';
 import Ripple from 'vue-ripple-directive';
+import WebFontLoader from 'webfontloader';
 
 Vue.directive('ripple', Ripple);
 export default {
@@ -24,10 +31,6 @@ export default {
 	data() {
 		return {
 			authenticated: false,
-			mock: {
-				username: "username",
-				password: "password"
-			}
 		}
 	},
 	mounted() {
@@ -51,16 +54,16 @@ body {
 	margin: 0;
 }
 
-#app {
+/* #app {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	color: #2c3e50;
-}
+} */
 
 main {
 	text-align: center;
-	margin-top: 40px;
+	/* margin-top: 40px; */
 }
 
 header {
@@ -82,7 +85,7 @@ header span {
 	padding-top: 16px;
 }
 
-.button {
+/* .button {
 	position: relative;
 	display: inline-block;
 	padding: 12px 24px;
@@ -101,17 +104,17 @@ header span {
 	cursor: pointer;
 	transition: all 0.15s ease;
 }
-.button:focus { outline: 0; }
+.button:focus { outline: 0; } */
 
 
 /* Button modifiers */
 
-.buttonBlue {
+ /* .buttonBlue {
 	background: #35495E;
 	text-shadow: 1px 1px 0 rgba(39, 110, 204, .5);
 }
 .buttonGray {
 	background: rgb(107, 108, 109);
-	text-shadow: 1px 1px 0 rgba(39, 110, 204, .5);
-}
+	text-shadow: 1px 1px 0 rgba(39, 110, 204, .5); 
+ }  */
 </style>
