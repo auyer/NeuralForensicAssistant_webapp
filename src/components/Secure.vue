2eef5e3d62d065df256e6b024a6198ab6fec2529
Rafael Passos <rafael@rcpassos.me>
2018-11-30 17:45:40 -0200

    Added text to secure area
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- src/components/Secure.vue
+++ src/components/Secure.vue
@@ -1,9 +1,22 @@
 <template>
     <div id="secure">
-        <h1>Secure Area</h1>
-        <p>
-            This is a secure area
-        </p>
+        <v-card class="pt-4 mx-auto" flat max-width="600" >
+            <v-card-text>
+                <h3 class="headline mb-2">
+                    Welcome
+                </h3>
+            </v-card-text>
+            <v-divider></v-divider>
+            <h3 class="headline mb-2 mx-auto">
+                {{ mock.name }}
+            </h3>
+            <v-layout tag="v-card-text" text-md wrap >
+                <v-flex tag="strong" xs5 text-xs-right mr-1 mb-2>Position : </v-flex>
+                <v-flex>{{ mock.position }}</v-flex>
+                <v-flex tag="strong" xs5 text-xs-right mr-1 mb-2>Agency: </v-flex>
+                <v-flex>{{ mock.agency }}</v-flex>
+            </v-layout>
+        </v-card>
     </div>
 </template>
 
@@ -11,7 +24,13 @@
     export default {
         name: 'Secure',
         data() {
-            return {};
+            return {
+                mock: {
+                    name : "Javier Gonzales ",
+                    position : "Chief Cyber Foresics Investigator",
+                    agency : "CYBERPOL Investigation",
+                }
+            };
         }
     }
 </script>
